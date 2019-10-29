e7a0624008146749d20c7611612fc28d3f9e5cde
EvgenSk <evgensk@gmail.com>
2019-10-29 23:04:08 +0700

    add address annotations (Server-side validation)

--- save-points/00-Starting-point/BlazingPizza.Shared/Address.cs
+++ save-points/00-Starting-point/BlazingPizza.Shared/Address.cs
@@ -5,17 +5,17 @@ namespace BlazingPizza
     public class Address
     {
         public int Id { get; set; }
-
+		[Required, MaxLength(100)]
         public string Name { get; set; }
-
+		[Required, MaxLength(100)]
         public string Line1 { get; set; }
-
-        public string Line2 { get; set; }
-
-        public string City { get; set; }
-
-        public string Region { get; set; }
-
-        public string PostalCode { get; set; }
+		[Required]
+		public string Line2 { get; set; }
+		[Required, MaxLength(50)]
+		public string City { get; set; }
+		[Required, MaxLength(20)]
+		public string Region { get; set; }
+		[Required, MaxLength(20)]
+		public string PostalCode { get; set; }
     }
 }
