11c74861d69ff6c32addd40c504faae4c3326807
EvgenSk <evgensk@gmail.com>
2019-11-01 23:14:08 +0700

    Finish with Session #6

--- save-points/00-Starting-point/BlazingPizza.Server/OrdersController.cs
+++ save-points/00-Starting-point/BlazingPizza.Server/OrdersController.cs
@@ -24,8 +24,8 @@ namespace BlazingPizza.Server
         public async Task<ActionResult<List<OrderWithStatus>>> GetOrders()
         {
             var orders = await _db.Orders
-                // .Where(o => o.UserId == GetUserId())
-                .Include(o => o.DeliveryLocation)
+				.Where(o => o.UserId == GetUserId())
+				.Include(o => o.DeliveryLocation)
                 .Include(o => o.Pizzas).ThenInclude(p => p.Special)
                 .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
                 .OrderByDescending(o => o.CreatedTime)
@@ -39,8 +39,8 @@ namespace BlazingPizza.Server
         {
             var order = await _db.Orders
                 .Where(o => o.OrderId == orderId)
-                // .Where(o => o.UserId == GetUserId())
-                .Include(o => o.DeliveryLocation)
+				.Where(o => o.UserId == GetUserId())
+				.Include(o => o.DeliveryLocation)
                 .Include(o => o.Pizzas).ThenInclude(p => p.Special)
                 .Include(o => o.Pizzas).ThenInclude(p => p.Toppings).ThenInclude(t => t.Topping)
                 .SingleOrDefaultAsync();
@@ -59,12 +59,12 @@ namespace BlazingPizza.Server
 			await Task.Delay(5000);
             order.CreatedTime = DateTime.Now;
             order.DeliveryLocation = new LatLong(51.5001, -0.1239);
-            // order.UserId = GetUserId();
+			order.UserId = GetUserId();
 
-            // Enforce existence of Pizza.SpecialId and Topping.ToppingId
-            // in the database - prevent the submitter from making up
-            // new specials and toppings
-            foreach (var pizza in order.Pizzas)
+			// Enforce existence of Pizza.SpecialId and Topping.ToppingId
+			// in the database - prevent the submitter from making up
+			// new specials and toppings
+			foreach (var pizza in order.Pizzas)
             {
                 pizza.SpecialId = pizza.Special.Id;
                 pizza.Special = null;
